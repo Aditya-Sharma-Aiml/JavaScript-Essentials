49f5bcb50c35a05d2320fad0ef57056bd7ca61fb
Adita-Sharma-Aiml <adityasharma.cseaiml@gmail.com>
2025-04-30 21:45:51 +0530

    ep-01

---
+++ ep-01/index.js
@@ -0,0 +1,20 @@
+//template(string) literals:
+const age = 20 ;
+const fullname = "Aditya";
+
+// output:hello Mr Aditya your age is: 20
+console.log("hello Mr "+fullname+" your age is: "+age)
+
+console.log(`hello Mr ${fullname} your age is :${age}`)
+console.log(`hello Mr ${fullname} 
+your age is :${age}`)
+
+// add two number
+
+const a=10;
+const b = 20;
+console.log("sum of "+a+" and "+b+" is :"+a+b)
+
+console.log(`sum of ${a} and ${b} is : ${a+b}`)
+
+
